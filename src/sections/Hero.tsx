9ee2ea0766334021af31e1def1039b0c30c66e63
Wambui Ndungu <wambuin18@gmail.com>
2024-10-31 13:47:47 +0300

    create hero

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,30 @@
+import Button from "@/components/Button";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="bg-hero-img bg-cover bg-center bg-no-repeat h-[80vh]">
+      <div className="h-full bg-gradient-to-tr from-green via-green/50 to-transparent">
+        <div className="responsive h-full py-24 flex flex-col justify-end">
+          <div className="h-auto w-[50vw]">
+            <h1 className="text-white">
+              Sustainable Plumbing Solutions for Reliable, Safe, and Efficient
+              Systems
+            </h1>
+            <p className="text-white text-xl">
+              At Vicwa Plumbing Supplies and Solutions, we pride ourselves on delivering quality solutions that ensure the
+              seamless flow of water and waste systems.
+            </p>
+            <div className="py-4">
+              <Button id="services" variant="primary">
+                Explore our services
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
